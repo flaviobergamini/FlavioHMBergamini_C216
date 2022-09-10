41e12acf3ed945c915e61ff781008cd214f99ede
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-09-10 17:23:48 -0300

    Criando backend em Spring Boot

---
+++ lab_rest_server/src/main/java/br/inatel/labs/lab_rest_server/controller/CursoController.java
@@ -0,0 +1,77 @@
+package br.inatel.labs.lab_rest_server.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.inatel.labs.lab_rest_server.model.Curso;
+import br.inatel.labs.lab_rest_server.service.CursoService;
+
+@RestController
+@RequestMapping("/curso")
+public class CursoController {
+	
+	@Autowired
+	private CursoService servico;
+	
+	@GetMapping
+	public List<Curso> listar(){
+		List<Curso> listaCurso = servico.pesquisarCurso();
+		return listaCurso;
+	}
+	
+	@GetMapping("/{id}")
+	public Curso buscar(@PathVariable("id") Long cursoId) {
+		List<Curso> listaCurso = servico.buscarCursoPeloId(cursoId);
+		Curso curso;
+		if(listaCurso.size() > 0) {
+			curso = listaCurso.get(0);
+			return curso;
+		}
+		else {
+			String message = String.format("Nenhum curso encontrado com o id [%s]", cursoId);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+		}
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Curso criar(@RequestBody Curso curso) {
+		Curso cursoCriado = servico.criarCurso(curso);
+		return cursoCriado;
+	}
+	
+	@PutMapping
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void atualizar(@RequestBody Curso curso) {
+		servico.atualizarCurso(curso);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable("id") Long cursoId) {
+		boolean verifica = servico.removerCursoPeloId(cursoId);
+		
+		if(verifica == false) {
+			String message = String.format("Nenhum curso encontrado com o id [%s]", cursoId);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+		}
+	}
+	
+	@GetMapping("/pesquisa")
+	public List<Curso> listarPeloFragDescricao(@RequestParam("descricao") String fragDescricao){
+		return servico.pesquisarCursoPeloFragDescricao(fragDescricao);
+	}
+}
---
+++ lab_rest_server/src/main/java/br/inatel/labs/lab_rest_server/service/CursoService.java
@@ -0,0 +1,85 @@
+package br.inatel.labs.lab_rest_server.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+import br.inatel.labs.lab_rest_server.model.Curso;
+
+@Service
+public class CursoService {
+	
+	private List<Curso> listaDeCursos = new ArrayList<>();
+	
+	@PostConstruct
+	private void setup() {
+		Curso c1 = new Curso(1, "REST com Spring Boot", 20);
+		Curso c2 = new Curso(2, "Programação com Java 11", 80);
+		Curso c3 = new Curso(3, "Dominando a IDE Eclipse", 120);
+		Curso c4 = new Curso(4, "Programação com .NET 6.0", 70);
+		Curso c5 = new Curso(5, "REST com ASP.NET Core", 20);
+		
+		listaDeCursos.add(c1);
+		listaDeCursos.add(c2);
+		listaDeCursos.add(c3);
+		listaDeCursos.add(c4);
+		listaDeCursos.add(c5);
+	}
+	
+	public List<Curso> pesquisarCurso(){
+		return listaDeCursos;
+	}
+	
+	public List<Curso> buscarCursoPeloId(Long cursoId){
+		
+		List<Curso> lCursos = new ArrayList<>();
+		for(int i=0; i < listaDeCursos.size(); i++) {
+			if(listaDeCursos.get(i).getId() == cursoId) {
+				lCursos.add(listaDeCursos.get(i));
+			}
+		}
+		return lCursos;
+	}
+	
+	public Curso criarCurso(Curso curso) {
+		Long id = new Random().nextLong();
+		curso.setId(id);
+		listaDeCursos.add(curso);
+		return curso;
+	}
+	
+	public void atualizarCurso(Curso curso) {
+		listaDeCursos.remove(curso);
+		listaDeCursos.add(curso);
+	}
+	
+	public boolean removerCursoPeloId(Long cursoId) {
+		for(int i=0; i < listaDeCursos.size(); i++) {
+			if(listaDeCursos.get(i).getId() == cursoId) {
+				listaDeCursos.remove(i);
+				return true;
+			}
+			else
+				return false;
+		}
+		return false;
+	}
+	
+	public List<Curso> pesquisarCursoPeloFragDescricao(String fragDescricao){
+		if(Objects.isNull(fragDescricao) || fragDescricao.isBlank()) {
+			return List.of();
+		}
+		List<Curso> listaDeCursosEncontrados = this.listaDeCursos.stream()
+				.filter(c -> c.getDescricao().trim().toLowerCase().contains(fragDescricao.trim().toLowerCase()))
+				.collect(Collectors.toList())
+				;
+		return listaDeCursosEncontrados;
+	}
+}
